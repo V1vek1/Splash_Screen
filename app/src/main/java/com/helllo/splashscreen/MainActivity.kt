0841da2d1f52fed6662533d600a5fe38fb8e9461
Vivek kumar singh <vk692896@gmail.com>
2023-11-03 17:59:20 +0530

    14th and 15th Days App

--- app/src/main/java/com/helllo/splashscreen/MainActivity.kt
+++ app/src/main/java/com/helllo/splashscreen/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        //Yeh code se App ke upper me jo heading likha aata h n usko hum hide kar sakte, Per mere iss
+        //Yeh code se, App ke upper me jo heading likha aata h n usko hum hide kar sakte, Per mere iss
         //App me Already Heading Hidden hi thi to mujhe ye code likhne ki jarurat nahi h
         supportActionBar?.hide()  //Yeh "Question bar" isiliye lga h kyuki, Yeh btata h ki Kya ActionBar h
                                   //Agar h to Hide kardo or nahi h to kuch maat karo, Basically Yeh Kotlin me
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         Handler().postDelayed({   //Yha pr maine handler method ko call kiya phri usko "postDelayed" se
                                   //assign kiya phir usme ek "Intent" bnaya
 
-            val intent=  Intent(this, SecondActivity::class.java)
+            val intent=  Intent(this, SignUp::class.java)
             startActivity(intent)  //Yha "intent" ko Start kiya
             finish()               //Yha maine "finish" method ko call kiya, taki hum "Second Activity" se back
                                    //aane per phir se "Splash Screen" per na chale jaye, issliliye maine "finish"
--- app/src/main/java/com/helllo/splashscreen/SignUp.kt
+++ app/src/main/java/com/helllo/splashscreen/SignUp.kt
@@ -3,7 +3,7 @@ package com.helllo.splashscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SecondActivity : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
